2f8dfda64105d7d763c922589135cd19b55b1b81
Pranavsk22 <skpranav22.7@gmail.com>
2025-07-09 09:49:00 +0530

    fix: set model cache paths for Hugging Face Docker Space

--- backend/nlp_modules/embedder.py
+++ backend/nlp_modules/embedder.py
@@ -1,4 +1,9 @@
 from sentence_transformers import SentenceTransformer
+import os
+
+os.environ["HF_HOME"] = "/home/user/.cache"
+os.environ["TRANSFORMERS_CACHE"] = "/home/user/.cache/transformers"
+os.environ["SENTENCE_TRANSFORMERS_HOME"] = "/home/user/.cache/sentence_transformers"
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
